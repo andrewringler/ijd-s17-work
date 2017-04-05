e2dcf58b6c64282ca91300e9da030ed37de2ecc3
Andrew Ringler <andrew@andrewringler.com>
2017-04-05 17:33:08 -0400

    student work, example

---
+++ assignment2/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(500, 500);
+
+    // a red circle!
+    // it still has the green stroke
+    // outline from above
+    fill(255, 0, 0);
+    ellipse(250, 250, 150, 150);
+}
